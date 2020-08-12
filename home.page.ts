599e8774129ad9b517c6419fee0e261e8ac9d481
PankajDas7903905495 <69471874+PankajDas7903905495@users.noreply.github.com>
2020-08-12 19:12:09 +0530

    Add files via upload

---
+++ home.page.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { FileChooser, FileChooserOptions } from '@ionic-native/file-chooser/ngx';
+import { FilePath } from '@ionic-native/file-path/ngx';
+import { toBase64String } from '@angular/compiler/src/output/source_map';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  file:any=[];
+  returnpath:string="";
+  constructor(public filePath:FilePath,public fileChooser:FileChooser) {
+
+
+  }
+
+PickFile()
+{
+  this.fileChooser.open().then((fileuri)=>{
+    this.filePath.resolveNativePath(fileuri).then((resolvednativepath)=>{
+      this.returnpath = resolvednativepath;
+    })
+  })
+
+}
+}
